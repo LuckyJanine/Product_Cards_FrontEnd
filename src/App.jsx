8597e68742b795e26ae7a81ba167c3965ece2f71
LJ_Tao <lingjie.tao63@gmail.com>
2025-07-07 18:13:43 +0200

    Home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,41 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import MovieCard from "./components/MovieCard"
+import Home from "./pages/Home"
 
 function App() {
 
-  const movieNumber = 2;
-
   return (
     <>
-      {movieNumber === 1 ? (
-            <MovieCard movie={
-              {
-                title: "Tim's Film",
-                release_date: "2024"
-              }
-            }
-          />
-        ) : (
-            <MovieCard movie={
-              {
-                title: "Joes's Film",
-                release_date: "2020"
-              }
-            }
-          />
-        )
-      }
+      <Home/>
     </>
-  )
-}
-
-function Text({display}){
-  return (
-    <div>
-      <p>{display}</p>
-    </div>
   )
 }
 
