c776e355a141d54486be6d8fe21892fe2f80ebfb
LJ_Tao <lingjie.tao63@gmail.com>
2025-07-28 15:38:34 +0200

    use html5 semantic header element

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
-import './App.css'
+import './css/App.css'
 import Home from "./pages/Home"
 import Favorites from "./pages/Favorites"
 import {Routes, Route} from 'react-router-dom'
+import NavigationBar from './components/NavigationBar'
 
 function App() {
 
   return (
-    <main className='main-content'>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/favorites' element={<Favorites/>}/>
-      </Routes>
-    </main>
+    <div>
+
+      <header>
+        <NavigationBar />
+      </header>
+      
+      <main className='main-content'>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/favorites' element={<Favorites/>}/>
+        </Routes>
+      </main>
+      
+    </div>
   )
 }
 
